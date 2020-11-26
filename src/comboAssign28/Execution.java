32a6b594ab6b77282f770e2c121215167fb49466
Ashish Malla <ashishmalla45@gmail.com>
2020-11-25 18:44:05 -0700

    added code

--- src/comboAssign28/Execution.java
+++ src/comboAssign28/Execution.java
@@ -6,54 +6,52 @@ public class Execution {
 	public static void main(String[] args) {
 		Functions obj = new Functions();
 		Scanner input = new Scanner(System.in);
-		
-		String decision ="";
+
+		String decision = "";
 		do {
 			System.out.println("Enter your choice: prime | nthPrime | allPrime");
 			String choice = input.next();
-			
+
 			switch (choice) {
-			
+
 			case "prime": {
 				System.out.println("Enter num to see if it's a prime num: ");
-				int num= input.nextInt(); 
+				int num = input.nextInt();
 				boolean isPrime = obj.isPrimeNum(num);
 				if (isPrime) {
-					System.out.println(num+ " it's a prime number");
+					System.out.println(num + " it's a prime number");
+				} else {
+					System.out.println(num + " not a prime number");
 				}
-				else {
-					System.out.println(num+ " not a prime number");
-				}
-			break;
+				break;
 			}
-			
+
 			case "nthPrime": {
 				System.out.println("Enter num to find the first nth prime num: ");
-				int num= input.nextInt(); 
+				int num = input.nextInt();
 				obj.firstNthPrime(num);
-			break;
+				break;
 			}
-			
+
 			case "allPrime": {
 				System.out.println("From what num do you want to start the prime number search? : ");
 				int start = input.nextInt();
 				System.out.println("Till what num do you want to search the prime number?");
 				int end = input.nextInt();
 				obj.allPrimeBetween(start, end);
-			break;
+				break;
 			}
-			
+
 			default:
 				System.out.println("Wrong Input!");
 				break;
 			}
 			System.out.println("Do you wish to continue? Enter yes || no");
 			decision = input.next();
-		}while(decision.equalsIgnoreCase("yes"));
+		} while (decision.equalsIgnoreCase("yes"));
 		System.out.println("The End");
-		
-	
+
+		input.close();
 	}
-	
 
 }
